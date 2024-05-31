7b7ff51a085659443b9812bf3e24686998f39c74
Jess South <45018900+jes-so@users.noreply.github.com>
2024-05-31 15:21:54 -0400

    kinesisSink method cleanup (#26)
    * reduce command reuse
    
    * undo rebuildKinesisClient edit

--- src/main/java/io/jenkins/plugins/cdevents/sinks/KinesisSink.java
+++ src/main/java/io/jenkins/plugins/cdevents/sinks/KinesisSink.java
@@ -36,8 +36,8 @@ public class KinesisSink extends CDEventsSink {
                     "Jenkins plugin aws-java-sdk or aws-java-sdk-kinesis must be installed to use Kinesis sink");
         }
 
-        if (CDEventsGlobalConfig.get().getKinesisStreamName() == null
-                || CDEventsGlobalConfig.get().getKinesisStreamName().trim().isEmpty()) {
+        streamName = CDEventsGlobalConfig.get().getKinesisStreamName();
+        if (streamName == null || streamName.trim().isEmpty()) {
             throw new NullPointerException("Kinesis stream name cannot be blank");
         }
 
