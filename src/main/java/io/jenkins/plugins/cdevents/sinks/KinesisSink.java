6e9ace1c789d65f671140c9438cd7818a024574d
javedbaig <baigjaved38@gmail.com>
2024-06-06 19:39:11 +0100

    Worked in readability of code

--- src/main/java/io/jenkins/plugins/cdevents/sinks/KinesisSink.java
+++ src/main/java/io/jenkins/plugins/cdevents/sinks/KinesisSink.java
@@ -2,7 +2,6 @@
  * Copyright FMR LLC <opensource@fidelity.com>
  * SPDX-License-Identifier: Apache-2.0
  */
-
 package io.jenkins.plugins.cdevents.sinks;
 
 import com.amazonaws.auth.STSAssumeRoleSessionCredentialsProvider;
@@ -32,48 +31,59 @@ public class KinesisSink extends CDEventsSink {
     private volatile static String iamRole;
 
     public KinesisSink() {
+        //Constructs a new instance of the KinesisSink
         if (Jenkins.get().getPlugin("aws-java-sdk") == null
                 && Jenkins.get().getPlugin("aws-java-sdk-kinesis") == null) {
             throw new NoClassDefFoundError(
-                    "Jenkins plugin aws-java-sdk or aws-java-sdk-kinesis must be installed to use Kinesis sink");
+                    "Jenkins plugin aws-java-sdk " +
+                            "or aws-java-sdk-kinesis " +
+                            "must be installed to use Kinesis sink");
         }
 
         streamName = CDEventsGlobalConfig.get().getKinesisStreamName();
         if (StringUtils.isBlank(streamName)) {
-            throw new NullPointerException("Kinesis stream name cannot be blank");
+            throw new NullPointerException(
+                    "Kinesis stream name cannot be blank");
         }
 
         rebuildKinesisClient();
     }
 
     public static synchronized void nullifyKinesisClient() {
+        // Sets the Kinesis client instance to null.
         kinesis = null;
     }
 
     public static synchronized void rebuildKinesisClient() {
+        // Build and instantiate the Kinesis client
         if (kinesis == null) {
-            streamName = CDEventsGlobalConfig.get().getKinesisStreamName().trim();
+            streamName = CDEventsGlobalConfig.get()
+                    .getKinesisStreamName().trim();
             region = CDEventsGlobalConfig.get().getKinesisRegion();
             endpoint = CDEventsGlobalConfig.get().getKinesisEndpoint();
             iamRole = CDEventsGlobalConfig.get().getIamRole();
             String roleSessionName = "cdevents-plugin";
 
-            AmazonKinesisClientBuilder kinesisBuilder = AmazonKinesisClientBuilder.standard();
+            AmazonKinesisClientBuilder kinesisBuilder =
+                    AmazonKinesisClientBuilder.standard();
             if (!StringUtils.isBlank(region)) {
                 kinesisBuilder.withRegion(region);
             }
             if (!StringUtils.isBlank(region)) {
-                AwsClientBuilder.EndpointConfiguration endpointConfiguration = new AwsClientBuilder.EndpointConfiguration(
+                AwsClientBuilder.EndpointConfiguration endpointConfiguration =
+                        new AwsClientBuilder.EndpointConfiguration(
                         endpoint, region);
                 kinesisBuilder.withEndpointConfiguration(endpointConfiguration);
             }
             if (!StringUtils.isBlank(iamRole)) {
-                STSAssumeRoleSessionCredentialsProvider credentialsProvider = new STSAssumeRoleSessionCredentialsProvider.Builder(
+                STSAssumeRoleSessionCredentialsProvider credentialsProvider =
+                        new STSAssumeRoleSessionCredentialsProvider.Builder(
                         iamRole, roleSessionName).build();
                 kinesisBuilder.withCredentials(credentialsProvider);
             }
 
-            LOGGER.info("Instantiating new Kinesis client {stream={}, region={}, endpoint={}, iamRole={}",
+            LOGGER.info("Instantiating new Kinesis client" +
+                            " {stream={}, region={}, endpoint={}, iamRole={}",
                     streamName, region, endpoint, iamRole);
             kinesis = kinesisBuilder.build();
         }
@@ -84,12 +94,13 @@ public class KinesisSink extends CDEventsSink {
         PutRecordRequest putRecordRequest = new PutRecordRequest();
         putRecordRequest.setStreamName(streamName);
 
-        ByteBuffer serialized = ByteBuffer.wrap(new JsonFormat().serialize(cloudEvent));
+        ByteBuffer serialized =
+                ByteBuffer.wrap(new JsonFormat().serialize(cloudEvent));
         putRecordRequest.setData(serialized);
 
         putRecordRequest.setPartitionKey(cloudEvent.getType());
 
         PutRecordResult result = kinesis.putRecord(putRecordRequest);
-        LOGGER.info("Kinesis putRecord result: {}",result);
+        LOGGER.info("Kinesis putRecord result: {}", result);
     }
 }
